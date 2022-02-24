98a4b96dcc0c803de1bc77e2861f239ff3ec4a67
Sam Williams <samwilliams281@gmail.com>
2022-02-24 16:33:51 -0500

    display api response data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,15 +1,85 @@
-var getUserRepos = function(user) {
-// format the github api url
-var apiUrl = "https://api.github.com/users/" + user + "/repos";
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
 
-// make a request to the url
-fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
-        console.log(data);
-    });
-  });
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  console.log(event);
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
 };
 
-getUserRepos("facebook");
-getUserRepos("microsoft");
-// getUserRepos();
+
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+
+  // make a get request to url
+  fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function(data) {
+          console.log(data);
+          displayRepos(data, user);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    })
+    .catch(function(error) {
+      alert('Unable to connect to GitHub');
+    });
+};
+  
+var displayRepos = function(repos, searchTerm) {
+  repoContainerEl.textContent = "";
+
+  if (repos.length === 0) {
+    repoContainerEl.textContent = 'No repositories found.';
+    return;
+  }
+  
+  repoSearchTerm.textContent = searchTerm;
+
+  for (var i =0; i < repos.length; i++) {
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+    var repoEl = document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+
+    repoEl.appendChild(titleEl);
+
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+
+    if (repos[i].open_issues_count > 0) {
+      statusEl.innerHTML = 
+      "<i class= 'fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+    } else {
+      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+
+    repoEl.appendChild(statusEl);
+
+    repoContainerEl.appendChild(repoEl);
+  }
+
+  console.log(repos);
+  console.log(searchTerm);
+};
+
+  userFormEl.addEventListener("submit", formSubmitHandler);
+  
